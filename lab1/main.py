9ee6222bce23e4f7a618d4d327fa69a4ffd40d9a
psztefko <p.sztefko@gmail.com>
2021-10-19 16:02:35 +0200

    task c

--- lab1/main.py
+++ lab1/main.py
@@ -11,9 +11,13 @@ def task_b1(numbers):
 def task_b2(numbers):
     return [number*2 for number in numbers]
 
+def task_c(list):
+    for i in range(len(list)):
+        print(i) if i%2 == 0 else None
+
 if __name__ == '__main__':
 
     #task_a(['Bartek', 'Ania', 'Kuba', 'Kasia', 'Maciek'])
     #task_b1([3, 2, 1, 5, 7])
     #print(task_b2([3, 2, 1, 5, 7]))
-    
+    task_c([element for element in range(10)])
