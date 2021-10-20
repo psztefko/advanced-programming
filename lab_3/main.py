3678d04f858edc606fc843ca7e271ec85612f26b
psztefko <p.sztefko@gmail.com>
2021-10-20 15:46:59 +0200

    func_3

--- lab_3/main.py
+++ lab_3/main.py
@@ -6,8 +6,13 @@ def func_1(name: str, surname: str) -> str:
 def func_2(num1: int, num2: int) -> int:
     return num1 * num2
 
+def func_3(num: int) -> str:
+    bool = True if num % 2 == 0 else False
+    return 'Liczba parzysta' if bool else 'Liczba nieparzysta'
+
 
 if __name__ == '__main__':
 
     func_1('Adam', 'Kowalski')
     func_2(3, 4)
+    func_3(6)
