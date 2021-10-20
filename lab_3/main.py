7de4a47e2573b4b73513b6d18e21d78ee8a9fcc8
psztefko <p.sztefko@gmail.com>
2021-10-20 15:38:12 +0200

    func_2

--- lab_3/main.py
+++ lab_3/main.py
@@ -3,6 +3,10 @@
 def func_1(name: str, surname: str) -> str:
     return f'Cześć {name} {surname}!'
 
+def func_2(num1: int, num2: int) -> int:
+    return num1 * num2
+
 if __name__ == '__main__':
 
-    print(func_1('Adam', 'Kowalski'))
+    func_1('Adam', 'Kowalski')
+    func_2(3, 4)
