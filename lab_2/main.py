bebd3741ac2d01f3cc8f5f4eb69cde64fc3d68a1
psztefko <p.sztefko@gmail.com>
2021-11-03 16:03:45 +0100

    compliant with PEP-8

--- lab_2/main.py
+++ lab_2/main.py
@@ -3,19 +3,24 @@ def task_a(names):
     for name in names:
         print(name)
 
+
 def task_b1(numbers):
     for i in range(len(numbers)):
         numbers[i] *= 2
     return numbers
 
+
 def task_b2(numbers):
     return [number*2 for number in numbers]
 
+
 def task_c():
-    print([element for element in range(10) if element%2 == 0])
+    print([element for element in range(10) if element % 2 == 0])
+
 
 def task_d():
-    print([element for element in range(10) if int(element)%2 == 0])
+    print([element for element in range(10) if int(element) % 2 == 0])
+
 
 if __name__ == '__main__':
 
