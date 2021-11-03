c090b5f4daac5a91bebe807f72ffcdfddf1e7e9e
psztefko <p.sztefko@gmail.com>
2021-11-03 16:01:56 +0100

    compliant with PEP-8

--- lab_3/main.py
+++ lab_3/main.py
@@ -3,22 +3,28 @@
 def func_1(name: str, surname: str) -> str:
     return f'Cześć {name} {surname}!'
 
+
 def func_2(num1: int, num2: int) -> int:
     return num1 * num2
+
 
 def func_3(num: int) -> str:
     bool = True if num % 2 == 0 else False
     return 'Liczba parzysta' if bool else 'Liczba nieparzysta'
 
+
 def func_4(num1: int, num2: int, num3: int) -> bool:
     return True if num1*num2 > num3 else False
+
 
 def func_5(list: list, num: int) -> bool:
     return True if num in list else False
 
+
 def func_6(list1: list, list2: list) -> list:
     list1.extend(list2)
     return [element ** 3 for element in list(dict.fromkeys(list1))]
+
 
 if __name__ == '__main__':
 
