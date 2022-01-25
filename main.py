1db936b05d14d0d7f90cc900c5bec94c7eb9df0a
psztefko <p.sztefko@gmail.com>
2022-01-26 00:05:28 +0100

    added iteration thought images and text on image

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
-#import numpy as np
-#import tensorflow as tf
 import cv2 as cv
 import imutils
+from pathlib import Path
 
+
+def write_text(image, counter):
+    font = cv.FONT_HERSHEY_COMPLEX
+    cv.putText(image, f'Na obrazku wykryto {counter} osoby', (60, 20), font, 0.5, (122, 20, 180), 2)
+
+    return image
 
 # Initializing the HOG person detector
 hog = cv.HOGDescriptor()
 hog.setSVMDetector(cv.HOGDescriptor_getDefaultPeopleDetector())
 
-# Reading the Image
-image = cv.imread('side.png')
+images = Path('images').rglob('*.png')
+for path in images:
 
-# Resizing the Image
-image = imutils.resize(image,width=min(400, image.shape[1]))
+    # Reading the Image
+    image = cv.imread(str(path))
 
-# Detecting all the regions in the image that has a pedestrians inside it
-(regions, _) = hog.detectMultiScale(image,winStride=(4, 4),padding=(4, 4),scale=1.05)
+    # Resizing the Image
+    image = imutils.resize(image,width=min(400, image.shape[1]))
 
-# Drawing the regions in the Image
-counter = 0
-for (x, y, w, h) in regions:
-    cv.rectangle(image, (x, y),(x + w, y + h),(0, 0, 255), 2)
-    counter += 1
+    # Detecting all the regions in the image that has a pedestrians inside it
+    (regions, _) = hog.detectMultiScale(image,winStride=(4, 4),padding=(4, 4),scale=1.05)
 
-print(f'Na obrazku wykryto {counter} osób')
+    # Drawing the regions in the Image
+    counter = 0
+    for (x, y, w, h) in regions:
+        cv.rectangle(image, (x, y),(x + w, y + h),(0, 0, 255), 2)
+        counter += 1
 
-# Showing the output Image
-cv.imshow('Image', image)
-cv.waitKey(0)
-cv.destroyAllWindows()
+    image = write_text(image, counter)
+    #print(f'Na obrazku wykryto {counter} osób')
+
+    # Showing the output Image
+    cv.imshow('Image', image)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
